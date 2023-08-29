a22d12decccd80d2c2218254592f28a800646d68
jerrysun0303 <jerry.sun_1@philips.com>
2023-08-28 20:37:32 -0700

    drivers/gpio: change the rightcopy of mxc_gpio.c
    only change the rightcopy for mxc_gpio.c file.
    
    
    Signed-off-by: jerrysun0303 <jerry.sun_1@philips.com>

--- drivers/gpio/mxc_gpio.c
+++ drivers/gpio/mxc_gpio.c
@@ -1,18 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (c) 2023 Koninklijke Philips N.V. All Rights Reserved.
+ * Copyright (C) 2009
+ * Guennadi Liakhovetski, DENX Software Engineering, <lg@denx.de>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2011
+ * Stefano Babic, DENX Software Engineering, <sbabic@denx.de>
  */
 
 #include <common.h>
